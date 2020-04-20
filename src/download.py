09061ead6e420270da76232abb3bc28c2a741806
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-04-20 23:52:20 +0900

    Add source codes and configuration files

---
+++ src/download.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This script is for downloading data essential to make a gene matrix.
+"""
+
+import os
+import yaml
+
+# Path settings
+project_dir = os.path.dirname(os.path.abspath('.'))
+conf_dir = os.path.join(project_dir, 'conf')
+filepath_conf_path = os.path.join(conf_dir, 'filepaths.yaml')
+fileurl_conf_path = os.path.join(conf_dir, 'fileurls.yaml')
+
+with open(filepath_conf_path) as filepath_conf_file:
+    filepath_conf = yaml.safe_load(filepath_conf_file)
+
+with open(fileurl_conf_path) as fileurl_conf_file:
+    fileurl_conf = yaml.safe_load(fileurl_conf_file)
+
+data_dir = os.path.join(project_dir, filepath_conf['DATA_DIR'])
+os.makedirs(data_dir)  # Raise FileExistsError if this directory already exists.
+
+# Download data
+for data_key in fileurl_conf:
+    cmd = f'wget -P {data_dir} {fileurl_conf[data_key]}'
+    print(cmd)
+    os.system(cmd)
